4883f6ebaa42b2c41869732369b4ad5a22ece3a1
johnpalowitch <johnpalowitch@gmail.com>
2017-07-12 18:40:45 -0400

    Added background checking. Also added LFR sims code.

--- recursive_mod.R
+++ recursive_mod.R
@@ -1,4 +1,4 @@
-recursive_mod <- function (fn, alpha = 0.05, data_dir = ".",
+recursive_mod <- function (fn, alpha = 0.05,
                            compare_path = "cb-signi", 
                            mod_type = "louvain") {
 
@@ -24,6 +24,7 @@ recursive_mod <- function (fn, alpha = 0.05, data_dir = ".",
     sg_fn_base <- paste0("subgraph", sgn)
     graph_fn <- file.path(paste0(sg_fn_base, ".dat"))
     G <- graph.edgelist(as.matrix(read.table(graph_fn)), directed = FALSE)
+    n <- length(V(G))
     if (mod_type == "louvain") {
       res <- cluster_louvain(G)
     } else {
@@ -35,21 +36,21 @@ recursive_mod <- function (fn, alpha = 0.05, data_dir = ".",
     comms_text <- unlist(lapply(comms, paste, collapse = " "))
     writeLines(comms_text, con = comms_fn)
     
-    if (length(comms) > 1 && length(comms) < length(V(G))) {
+    if (length(comms) > 1 && length(comms) < n) {
     
       # Compute c-b-scores
-      signi_dir <- paste0(sg_fn_base, "_signi")
+      signi_dir <- "signi-files"
       dir.create(signi_dir)
       setwd(signi_dir)
       system2(file.path(oldwd, compare_path, "compare"), 
-              paste(paste("-f", file.path("../", graph_fn)), 
-                    paste("-c", file.path("../", comms_fn)), 
+              paste(paste("-f", file.path("..", graph_fn)), 
+                    paste("-c", file.path("..", comms_fn)), 
                     "-t 0.01"))
       setwd("../")
       
     } else {
       
-      writeLines(paste(1, length(V(G)), 1, 1, 0, 0), con = paste0(sg_fn_base, ".dat.table"))
+      writeLines(paste(1, n, 1, 1, 0, 0), con = paste0(sg_fn_base, ".dat.table"))
       
     }
     
@@ -57,7 +58,7 @@ recursive_mod <- function (fn, alpha = 0.05, data_dir = ".",
     cbscores <- read.table(paste0(sg_fn_base, ".dat.table"), sep = "", 
                            header = FALSE)
     which_write <- which(cbscores$V4 <= alpha)
-    next_level_dir <- file.path("../", paste0("level", level + 1))
+    next_level_dir <- file.path("..", paste0("level", level + 1))
     if (!dir.exists(next_level_dir) && length(which_write) > 0)
       dir.create(next_level_dir)
     cur_next_nsgs <- length(list.files(next_level_dir))
@@ -69,11 +70,63 @@ recursive_mod <- function (fn, alpha = 0.05, data_dir = ".",
                   quote = FALSE, row.names = FALSE, col.names = FALSE)
     }
     
+    # Assess background (if non-trivial) and save
+    background <- unlist(comms[-which_write])
+    if (length(background) > 0 && length(background) < n) {
+      
+      # Getting significance of background
+      writeLines(paste(background, collapse = " "), con = "background.dat")
+      setwd(signi_dir)
+      system2(file.path(oldwd, compare_path, "compare"), 
+              paste(paste("-f", file.path("..", graph_fn)), 
+                    paste("-c", file.path("..", "background.dat")), 
+                    "-t 0.01"))
+      setwd("../")
+      cbscores <- read.table(paste0(sg_fn_base, ".dat.table"), sep = "", 
+                           header = FALSE)
+      
+      # If significant,
+      if (cbscores$V4 <= 0.05) {
+        
+        # Add to comms and save
+        comms <- c(comms, list(background))
+        comms_text <- unlist(lapply(comms, paste, collapse = " "))
+        writeLines(comms_text, con = comms_fn)
+        which_write <- c(which_write, length(comms))
+        
+        # re-compute c-b-scores for future reading
+        signi_dir <- paste0(sg_fn_base, "_signi")
+        dir.create(signi_dir)
+        setwd(signi_dir)
+        system2(file.path(oldwd, compare_path, "compare"), 
+                paste(paste("-f", file.path("..", graph_fn)), 
+                      paste("-c", file.path("..", comms_fn)), 
+                      "-t 0.01"))
+        setwd("../")
+        
+        # write the background subgraph
+        cur_next_nsgs <- length(list.files(next_level_dir))
+        next_sg_base <- paste0("subgraph", cur_next_nsgs + 1)
+        Gi <- delete.vertices(G, setdiff(1:n, comms[[length(comms)]]))
+        write.table(get.edgelist(Gi), sep = "\t",
+                    file = file.path(next_level_dir, 
+                                     paste0(next_sg_base, ".dat")),
+                    quote = FALSE, row.names = FALSE, col.names = FALSE)
+        
+      }
+        
+    }
+    
+    # Creating membership vector
+    membership <- numeric(n)
+    for (i in seq_along(which_write)) {membership[comms[which_write][[i]]] <- i}
+    return(membership)
+    
   }
   
   # Level loop
   level <- 1
-  
+  level_memships <- list(NULL)
   repeat {
     
     # Prepping level
@@ -85,16 +138,31 @@ recursive_mod <- function (fn, alpha = 0.05, data_dir = ".",
     }
     oldwd <- setwd(level_dir)
     
-    # Running on subgraphs
+    # Running on subgraphs and 
     nsgs <- length(list.files())
-    sapply(1:nsgs, level_run, level = level)
+    memships <- lapply(1:nsgs, level_run, level = level)
     
     # Check if next level was generated
     setwd(oldwd)
-    level <- level + 1
-    if (!dir.exists(file.path(mod_dir, paste0("level", level))))
+    if (!dir.exists(file.path(mod_dir, paste0("level", level + 1))))
       break
     
+    # Saving memberships
+    if (level == 1) {
+      level_memships[[level]] <- unlist(memships)
+    } else {
+      membership <- numeric(length(level_memships[[1]]))
+      for (j in seq_along(memships)) {
+        cindx <- which(level_memships[[level - 1]] == j)
+        membership[cindx] <- memships[[j]]
+        nonzeros <- cindx[which(memships[[j]] != 0)]
+        offset <- if (j > 1) {length(memships[[j - 1]])} else {0}
+        membership[nonzeros] <- membership[nonzeros] + offset
+      }
+    }
+    
+    level <- level + 1
+
   }
 
 }
